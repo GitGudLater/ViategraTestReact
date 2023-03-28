7663eace208a505527d5b6cf80ac77415018d638
Andrei Salauyou <Andrei_Salauyou@epam.com>
2023-03-28 11:28:35 +0300

    implemented input groups

--- vistegra-react-app/src/components/Input/Input.tsx
+++ vistegra-react-app/src/components/Input/Input.tsx
@@ -5,16 +5,19 @@ export const Input:FC = () => {
     return (
         <section className="input__container">
             <div className="input__element material">
-
+                <input type="radio" name='material'/>
+                <input type="radio" name='material'/>
             </div>
             <div className="input__element length">
-                <input type="text" />
+                <input type="number" min={1} max={10} step={0.5}/>
             </div>
             <div className="input__element width">
-                <input type="text" />
+                <input type="number" min={1} max={10} step={0.5}/>
             </div>
             <div className="input__element frame">
-
+                <input type="radio" name='frame'/>
+                <input type="radio" name='frame'/>
+                <input type="radio" name='frame'/>
             </div>
         </section>
     );
