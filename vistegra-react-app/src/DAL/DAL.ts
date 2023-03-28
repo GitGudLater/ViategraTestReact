67f8a56854853ed2df9c5612b5cc564ad8eab1cf
Andrei Salauyou <Andrei_Salauyou@epam.com>
2023-03-28 16:17:29 +0300

    implemented data fetching

--- vistegra-react-app/src/DAL/DAL.ts
+++ vistegra-react-app/src/DAL/DAL.ts
@@ -3,10 +3,19 @@ import { Fix } from '../models/config/fix.type';
 import { Frame } from '../models/config/frame.type';
 import { Material } from '../models/config/materials.type';
 import { Size } from '../models/config/size.type';
+import { Data } from '../models/data/data.type';
 
 const configUrl = '../assets/config.json';
 const dataUrl = '../assets/data.json';
 
+export const getData = async (): Promise<Data[]> => {
+    let materials: Data[] = [];
+    await axios.get(dataUrl).then(resp => materials = resp.data);
+    console.log(materials);
+    return materials;
+} 
+
+//TODO: replace multiple effect with one and fetching configs from store
 export const getMaterials = async (): Promise<Material[]> => {
     let configs: Material[] = [];
     await axios.get(configUrl).then(resp => resp.data.forEach((config:any) => {if(config.type === "material"){configs.push(config as Material)}}));
---
+++ vistegra-react-app/src/models/data/data.type.ts
@@ -0,0 +1,8 @@
+export interface Data {
+    type: string,
+    name: string,
+    material?: string,
+    unit: string,
+    width?: 1.5,
+    price: 15
+}
