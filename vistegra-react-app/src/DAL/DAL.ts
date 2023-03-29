abac6cf7ec581db2488e81fd4a024b83bb97c9d3
Andrei Salauyou <Andrei_Salauyou@epam.com>
2023-03-29 09:34:17 +0300

    implemented selectors for configs

--- vistegra-react-app/src/DAL/DAL.ts
+++ vistegra-react-app/src/DAL/DAL.ts
@@ -1,9 +1,5 @@
 import axios from 'axios'
 import { Config } from '../models/config/config.type';
-import { Fix } from '../models/config/fix.type';
-import { Frame } from '../models/config/frame.type';
-import { Material } from '../models/config/materials.type';
-import { Size } from '../models/config/size.type';
 import { Data } from '../models/data/data.type';
 
 const configUrl = '../assets/config.json';
@@ -16,34 +12,8 @@ export const getData = async (): Promise<Data[]> => {
     return materials;
 } 
 
-//TODO: replace multiple effect with one and fetching configs from store
-
 export const getConfigs = async (): Promise<Config[]> => {
     let configs: Config[] = [];
     await axios.get(configUrl).then(resp => configs = resp.data);
-    return configs;
-}
-
-export const getMaterials = async (): Promise<Material[]> => {
-    let configs: Material[] = [];
-    await axios.get(configUrl).then(resp => resp.data.forEach((config:any) => {if(config.type === "material"){configs.push(config as Material)}}));
-    return configs;
-}
-
-export const getFix = async (): Promise<Fix[]> => {
-    let configs: Fix[] = [];
-    await axios.get(configUrl).then(resp => resp.data.forEach((config:any) => {if(config.type === "fix"){configs.push(config as Fix)}}));
-    return configs;
-}
-
-export const getFrame = async (): Promise<Frame[]> => {
-    let configs: Frame[] = [];
-    await axios.get(configUrl).then(resp => resp.data.forEach((config:any) => {if(config.type === "frame"){configs.push(config as Frame)}}));
-    return configs;
-}
-
-export const getSize = async (): Promise<Size[]> => {
-    let configs: Size[] = [];
-    await axios.get(configUrl).then(resp => resp.data.forEach((config:any) => {if(config.type === "size"){configs.push(config as Size)}}));
     return configs;
 }
--- vistegra-react-app/src/store/configs/config.selectors.ts
+++ vistegra-react-app/src/store/configs/config.selectors.ts
@@ -1,6 +1,44 @@
+import { createSelector } from "reselect";
+import { Config } from "../../models/config/config.type";
+import { Fix } from "../../models/config/fix.type";
+import { Frame } from "../../models/config/frame.type";
+import { Material } from "../../models/config/materials.type";
+import { Size } from "../../models/config/size.type";
 import { RootState } from "../store";
 import { configAdapter } from "./configs.slice";
 
-export const configSelectors = configAdapter.getSelectors<RootState>(
-    (state) => state.configs
-  );
+const configState = (state: RootState) => state.configs;
+
+export const configEntitySelectors = configAdapter.getSelectors(configState)
+
+
+const selectSize = createSelector(configEntitySelectors.selectAll, ( configs) => {
+  let filteredConfigs: Size[] = [];
+  configs.forEach((config:Config) => {if(config.type === "size"){filteredConfigs.push({...config} as Size)}})
+  return filteredConfigs;
+});
+
+const selectFix = createSelector(configEntitySelectors.selectAll, ( configs) => {
+  let filteredFix: Fix[] = [];
+  configs.forEach((config:Config) => {if(config.type === "fix"){filteredFix.push({...config} as Fix)}})
+  return filteredFix;
+});
+
+const selectMaterial = createSelector(configEntitySelectors.selectAll, ( configs) => {
+  let filteredMaterial: Material[] = [];
+  configs.forEach((config:Config) => {if(config.type === "material"){filteredMaterial.push({...config} as Material)}})
+  return filteredMaterial;
+});
+
+const selectFrame = createSelector(configEntitySelectors.selectAll, ( configs) => {
+  let filteredFrame: Frame[] = [];
+  configs.forEach((config:Config) => {if(config.type === "frame"){filteredFrame.push({...config} as Frame)}})
+  return filteredFrame;
+});
+
+export const configSelectors = {
+  selectSize,
+  selectFix,
+  selectFrame,
+  selectMaterial,
+};
