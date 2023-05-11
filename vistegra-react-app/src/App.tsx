0dd8e1e62bf996c2a4b8bc45dddb247685f8179d
Andrei Salauyou <Andrei_Salauyou@epam.com>
2023-05-11 12:39:27 +0300

    Implemented parallax

--- vistegra-react-app/src/App.tsx
+++ vistegra-react-app/src/App.tsx
@@ -1,13 +1,35 @@
 import './App.scss';
 import { Input } from './components/Input/Input';
 import { Output } from './components/Output/Output';
+import { MouseParallaxContainer, MouseParallaxChild } from "react-parallax-mouse";
+
 
 function App() {
   return (
-    <div className="App app__container">
-      <Input/>
-      <Output/>
-    </div>
+    <MouseParallaxContainer 
+      className="App app__container parallax-container"
+      globalFactorX={0.4}
+      globalFactorY={0.4}
+      resetOnLeave>
+        <MouseParallaxChild 
+          className="parallax-bkg"
+          factorX={0.6}
+          factorY={0.1}>
+            <img src='../bkg/3.jpg'/>
+        </MouseParallaxChild>
+        <MouseParallaxChild
+          className="parallax"
+          factorX={0.1}
+          factorY={0.1}>
+          <Input/>
+        </MouseParallaxChild>
+        <MouseParallaxChild
+          className="parallax"
+          factorX={0.1}
+          factorY={0.1}>
+          <Output/>
+        </MouseParallaxChild>
+    </MouseParallaxContainer>
   );
 }
 
